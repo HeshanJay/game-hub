0279660a98f026e203ffb5e377699bb26cbf55b9
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-10-30 14:36:55 +0530

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,8 @@ export interface Platform {
 const useGames = (gameQuery: GameQuery) => useData<Game>("/games", {
     params: {
         genres: gameQuery.genre?.id, 
-        platforms: gameQuery.platform?.id
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder
     },
 }, [gameQuery]);
 
